0d69fb76be994f3892231b2842e11ddcaf888f1f
xavier <omondix214@gmail.com>
2022-12-12 16:25:08 +0300

    production env

--- config/prod.js
+++ config/prod.js
@@ -1,8 +1,9 @@
 require("dotenv").config();
 // prod.js - production keys here!!
 module.exports = {
-  googleClientID: process.env.googleClient,
-  googleClientSecret: process.env.googleClientSecret,
+  googleClientID:
+    "779829667489-18fiqvprvm43q7ksephuj7hufsukjqct.apps.googleusercontent.com",
+  googleClientSecret: "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",
   mongoURI: process.env.mongoURI,
   cookieKey: process.env.cookieKey,
 };
