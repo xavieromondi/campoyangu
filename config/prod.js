891ae1d12fdd483c656991883b30275aaa96a3f6
xavier <omondix214@gmail.com>
2022-12-13 14:30:46 +0300

    cookie key

--- config/prod.js
+++ config/prod.js
@@ -5,5 +5,5 @@ module.exports = {
     "779829667489-18fiqvprvm43q7ksephuj7hufsukjqct.apps.googleusercontent.com",
   googleClientSecret: "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",
   mongoURI: process.env.mongoURI,
-  cookieKey: "felix",
+  cookieKey: process.env.cookieKey,
 };
